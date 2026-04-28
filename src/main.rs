7fac012361f12e9dcbbca8fc7f3c9b0f5f64a37f
Your Name <you@example.com>
2026-04-28 11:52:16 -0400

    28/04/2026 17:52

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 mod backend;
-// mod tui;
+mod tui;
 use anyhow::Result;
 fn main() -> Result<()> {
-    let creator = backend::var_creator::VarCreator::new();
-    creator.create_key_pair("TestKeyPair", "/root/123")?;
-    creator.create_pk_file("TestPK", "/root/123.crt", "/root/123PK.auth")?;
-    let mut writer = backend::var_writer::VarWriter::new()?;
-    writer.write_pk_from_file("/root/123PK.auth")?;
-    // tui::run()
+    // let creator = backend::var_creator::VarCreator::new();
+    // creator.create_key_pair("TestKeyPair", "/root/123")?;
+    // creator.create_pk_file("TestPK", "/root/123.crt", "/root/123PK.auth")?;
+    // let mut writer = backend::var_writer::VarWriter::new()?;
+    // writer.write_pk_from_file("/root/123PK.auth")?;
+    tui::run();
     Ok(())
 }
+
 
 
